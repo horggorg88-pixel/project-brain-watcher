af66192adf16aca00465d4cf7933c450d4f06920
horggorg88-pixel <horggorg88@gmail.com>
2026-06-12 10:10:47 +0300

    chore: release watcher 1.4.19

--- apps/watcher-desktop/src/desktop-service-runner.ts
+++ apps/watcher-desktop/src/desktop-service-runner.ts
@@ -20,7 +20,7 @@ import {
 import { verifyProjectServerAccess } from './desktop-server-access.js';
 import { readServiceStatus, resolveServiceProfile } from './desktop-service-status.js';
 
-const WATCHER_PACKAGE = 'github:horggorg88-pixel/project-brain-watcher#v1.4.18';
+const WATCHER_PACKAGE = 'github:horggorg88-pixel/project-brain-watcher#v1.4.19';
 const SERVICE_ACTION_SETTLE_TIMEOUT_MS = 30_000;
 const SERVICE_ACTION_SETTLE_POLL_MS = 750;
 
